3c9f3b134f13cecc310dff9db7047f054801af4e
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-07-16 17:18:55 +0530

    Time: 3 ms (9.47%), Space: 9.5 MB (5.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int path(int i, int j, int n, int m, vector<vector<int>>& dp)
+    //Tabulation 
+    int paths(int n, int m)
     {
-        if(i>=n || j>=m) return 0;
+        vector<vector<int>>dp(n,vector<int>(m));
 
-        if(i == n-1 && j == m-1) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
+        dp[0][0] = 1;
 
-        return dp[i][j] = path(i,j+1,n,m,dp) + path(i+1,j,n,m,dp);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i>0) dp[i][j] = dp[i-1][j];
+                if(j>0) dp[i][j] += dp[i][j-1];
+            }
+        }
+
+        return dp[n-1][m-1];
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return path(0,0,m,n,dp);
+        return paths(m,n);
     }
 };
