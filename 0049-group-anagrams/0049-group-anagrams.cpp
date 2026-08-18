bcfcafd5d10981bc5365ad8938b298f62bf64850
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-08-19 00:39:51 +0530

    Time: 19 ms (44.5%), Space: 26.1 MB (50.92%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        int i = 0;
+        for(auto str : strs){
+            sort(str.begin(),str.end());
+            mp[str].push_back(strs[i++]);
+        }
+
+        vector<vector<string>>ans;
+        for(auto [k,v] : mp) ans.push_back(v);
+
+        return ans;
+    }
+};
