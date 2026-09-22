eab5fe4656514afe72bb921ea3dba11f96800881
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-09-23 02:58:24 +0530

    Time: 0 ms (100%), Space: 12.6 MB (5.51%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,14 +3,15 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         int n = arr.size();
 
-        unordered_map<int,int>mp;
-        for(auto x : arr) mp[x]++;
+        int freq[2001] = {0};
+        for(int i = 0; i < n; i++) {
+            freq[arr[i] + 1000]++;
+        }
 
-        unordered_set<int>st;
-        for(auto& [k,v] : mp)
-        {
-            if(st.count(v)) return false;
-            st.insert(v);
+        sort(freq, freq + 2001);
+
+        for(int i = 1; i < 2001; i++){
+            if(freq[i] != 0 && freq[i] == freq[i-1]) return false;
         }
 
         return true;
