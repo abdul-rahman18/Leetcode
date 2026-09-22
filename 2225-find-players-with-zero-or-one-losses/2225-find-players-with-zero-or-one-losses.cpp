a5758c6f7acefcad0120ffb38006aae9d770502b
Abdul Rahman <mohammedabdulrahman188@gmail.com>
2026-09-23 01:53:09 +0530

    Time: 201 ms (45.29%), Space: 181.9 MB (38.91%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -3,18 +3,21 @@ public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         int n = matches.size();
         unordered_map<int,int>mp;
-        sort(matches.begin(), matches.end());
         
         for(int i = 0; i < n; i++) mp[matches[i][1]]++;
 
         vector<vector<int>>ans(2);
-        for(auto [k,v] : mp){
-            if(v == 1) ans[1].push_back(k);
-        }
-
         for(auto x : matches)
         {
-            if(mp.find(x[0]) == mp.end() && (ans[0].size() == 0 || x[0] != ans[0].back())) ans[0].push_back(x[0]);
+            int winner = x[0];
+            int loser = x[1];
+
+            if(mp.find(winner) == mp.end()) {
+                ans[0].push_back(winner);
+                mp[winner] = 2;
+            }
+
+            if(mp[loser] == 1) ans[1].push_back(loser);
         }
 
         sort(ans[0].begin(), ans[0].end());
